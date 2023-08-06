0eca2af2f815a34f3dc9d83cefe4df6debe28791
Rahulshashank21 <rahulshashank21@gmail.com>
2023-08-06 16:43:11 +0530

    model training

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -7,6 +7,7 @@ sys.path.append("E:\\Daimond_price_prediction\\src")
 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 if __name__=='__main__':
     obj=DataIngestion()
@@ -14,4 +15,6 @@ if __name__=='__main__':
     print(train_data_path,test_data_path)
     data_transformation =  DataTransformation()
     train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
+    model_trainer=ModelTrainer()
+    model_trainer.initate_model_training(train_arr,test_arr)
   
